51ecc7d05c89ef2ff25b13eddf1f570687d3b744
vwucher <v.wucher@gmail.com>
2016-08-17 15:59:52 +0200

    getORFlength.pl

--- getORFlength.pl
+++ getORFlength.pl
@@ -1,3 +1,6 @@
+#!/usr/bin/perl -w
+
+
 # Perl libs
 use warnings;
 use strict;
@@ -15,20 +18,19 @@ use ExtractFromFeature;
 use Intersect;
 use Utils;
 use Orf;
+use ExtractCdnaOrf;
 use List::Util 'shuffle';
 
 
 ### Input variables
-my $fasta  = "";
-my $output = ""
+my $infile = "";
 my $type   = 0;
 
 ## Parse options and print usage if there is a syntax error,
 ## or if usage was explicitly requested.
 GetOptions(
     'i|infile=s' => \$infile,
-    't|type=i'   => \$type;
-    'o|output'   => \$output;
+    't|type=i'   => \$type,
     ) or pod2usage(2);
 
 # Test parameters
@@ -36,30 +38,30 @@ pod2usage("- Error: Cannot read your input FASTA file '$infile'...\n") unless( -
 pod2usage("- Error: ORF type is not valid. Integer in {0,1,2,3,4}.\n")     if( $type!=0 && $type!=1 && $type!=2 && $type!=3 && $type!=4 );
 
 
-my %h_orf;       # for storing and printing ORF sequence
+my %orf;         # for storing ORF
 my $orfFlag = 0; # get getTypeOrf result
 
 # Constantes
 my $strand  = ".";
-my $kmerMax = 0
+my $kmerMax = 0;
 
 # Create SeqIO objects
-my $seqin = Bio::SeqIO->new(-file => $fastaFile, -format => "fasta");
-# Open output file
-open FILEOUT, "> $output" or die "Error! Cannot access output file '". $output . "': ".$!;
+my $seqin = Bio::SeqIO->new(-file => $infile, -format => "fasta");
 
 # Go through each sequences
 while(my $seq = $seqin->next_seq())
 {
-    $orfFlag = &getTypeOrf($seq->id(), $seq->seq(), $strand, \%h_orf, $type, $kmerMax);
-
+    my $tr = $seq->id();
+    
+    $orfFlag = ExtractCdnaOrf::getTypeOrf($tr, $seq->seq(), $strand, \%orf, $type, $kmerMax);
+    
     if($orfFlag != -1)
     {
-	print FILEOUT $seq->id() ."\t". $h_orf->{'orflength'};
+	print $tr ."\t". length($orf{$tr}) ."\n";
     }
     else
     {
-	print FILEOUT $seq->id() ."\t". "-1";
+	print $tr ."\t". "-1" ."\n";
     }
 }
 
